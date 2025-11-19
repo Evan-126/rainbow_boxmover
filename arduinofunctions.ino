091ee11f9638e9715c645296ea001b22baea8fee
Evan J. Long <longevan217@gmail.com>
2025-11-18 23:25:33 -0600

    included serial on arduinofunctinos

--- arduinofunctions.ino
+++ arduinofunctions.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
+// motors init
 const int ENA = 3;
 const int R1  = 2;
 const int R2  = 4;
@@ -8,43 +9,50 @@ const int L1  = 6;
 const int L2  = 7;
 const int ENB = 5;
 
+//servo init
+Servo myServo;
+
 const int servoPin = 9;
 
+// HC-05 init
 #define TXD 10
 #define RXD 11
 
-Servo myServo;
 SoftwareSerial mySerial(RXD, TXD);  // RX, TX
 
+// ------------------------
+// Movement Functions
+// ------------------------
+
 void f() {
-  digitalWrite(R1, HIGH);
-  digitalWrite(R2, LOW);
-  digitalWrite(L1, HIGH);
-  digitalWrite(L2, LOW);
+  digitalWrite(R2, HIGH);
+  digitalWrite(R1, LOW);
+  digitalWrite(L2, HIGH);
+  digitalWrite(L1, LOW);
   delay(100);
 }
 
 void b() {
-  digitalWrite(R1, LOW);
-  digitalWrite(R2, HIGH);
-  digitalWrite(L1, LOW);
-  digitalWrite(L2, HIGH);
+  digitalWrite(R2, LOW);
+  digitalWrite(R1, HIGH);
+  digitalWrite(L2, LOW);
+  digitalWrite(L1, HIGH);
   delay(100);
 }
 
 void l() {
-  digitalWrite(R1, HIGH);
-  digitalWrite(R2, LOW);
-  digitalWrite(L1, LOW);
+  digitalWrite(R2, HIGH);
+  digitalWrite(R1, LOW);
   digitalWrite(L2, LOW);
+  digitalWrite(L1, LOW);
   delay(100);
 }
 
 void r() {
-  digitalWrite(R1, LOW);
   digitalWrite(R2, LOW);
-  digitalWrite(L1, HIGH);
-  digitalWrite(L2, LOW);
+  digitalWrite(R1, LOW);
+  digitalWrite(L2, HIGH);
+  digitalWrite(L1, LOW);
   delay(100);
 }
 
@@ -56,16 +64,22 @@ void s() {
   delay(100);
 }
 
-void o() {                // open = 90° clockwise
-  myServo.write(90);
+// ------------------------
+// Servo Open/Close
+// ------------------------
+void o() {             // open = 90° clockwise
+  myServo.write(80);
   delay(300);
 }
 
-void c() {                // close = 90° counterclockwise
-  myServo.write(0);
+void c() {             // close = 90° counterclockwise
+  myServo.write(160);
   delay(300);
 }
 
+// ------------------------
+// Setup
+// ------------------------
 void setup() {
   // Motor pins
   pinMode(ENA, OUTPUT);
@@ -78,15 +92,18 @@ void setup() {
   digitalWrite(ENA, HIGH);
   digitalWrite(ENB, HIGH);
 
-  s();  // stop motors at start
+  // s();  // stop motors at start
 
   // Servo
   myServo.attach(servoPin);
-  myServo.write(0);
+  myServo.write(80);
+  // c(); // add a little bit of flair
+  // o();
 
   // Bluetooth
   Serial.begin(9600);
   mySerial.begin(9600);
+
 }
 
 // ------------------------
@@ -94,4 +111,25 @@ void setup() {
 // ------------------------
 void loop() {
 
+  // bluetooth commands
+  if (mySerial.available()) {       // Check if data received from Bluetooth
+    char cmd = mySerial.read();     // Read single character
+
+    if (cmd == 'f') f();            // Move forward
+    else if (cmd == 'b') b();       // Move backward
+    else if (cmd == 'l') l();       // Turn left
+    else if (cmd == 'r') r();       // Turn right
+    else if (cmd == 's') s();       // Stop all motors
+    else if (cmd == 'o') o();       // Servo open
+    else if (cmd == 'c') c();       // Servo close
+  }
+  // // servo test
+  // o();
+  // c();
+  // delay(500);
+
+  // // motor test
+  // // f();
+  // // delay(1000);
+  // s();
 }
