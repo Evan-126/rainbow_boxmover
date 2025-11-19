49943b8697bee831155934b2daf01bfdff2fa4c6
Evan J. Long <longevan217@gmail.com>
2025-11-19 13:49:45 -0600

    serial_test.py works now, stuff moves.

--- serial_test.py
+++ serial_test.py
@@ -1,6 +1,12 @@
 import time as t
 import serial as ser
 
+#initial testing to make sure the COM is open
+import serial.tools.list_ports as lp
+ports = list(lp.comports())
+for p in ports:
+    print(p.device, p.description)
+
 # Arduino commands: change later with correct strings used in Arduino
 forward = 'F'
 backward = 'B'
@@ -12,7 +18,7 @@ close_claw = 'C'
 approach = 'A'
 
 # connecting to Arduino
-port_num = 'COM6' # change for use
+port_num = 'COM7' # change for use
 baud_rate = 9600 # change bc i want to make our lives harder
 
 # connect to serial monitor
@@ -30,11 +36,14 @@ def connect_to_Arduino(port=port_num, baud=baud_rate):
     
 # send commands
 def send_to_Arduino(arduino, command):
-    arduino.write((command + '\n').encode('utf-8')) # string converting string to bytes
-    print('sent ' + (command + '\n').encode('utf-8') + 'via serial')
-    t.sleep(0.05) # pause (50ms) to process command, can incrase to 0.1?
+    arduino.write(command.encode()) # string converting string to bytes
+    print('sent ' + (command) + 'via serial')
+    t.sleep(0.08) # pause (50ms) to process command, can incrase to 0.1?
 
 a = connect_to_Arduino(port_num, baud_rate)
+if a is None:
+    print("Could not connect to HC-05. Exiting.")
+    exit()
 
 # Move forward 10 times
 for _ in range(10):
