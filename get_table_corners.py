20581f8bf0f2e9ea70fff5d5d58f7a9efc18d471
carolinaolaya-cell <carolinaolaya@utexas.edu>
2025-11-20 00:23:28 -0600

    Add files via upload

--- get_table_corners.py
+++ get_table_corners.py
@@ -12,30 +12,35 @@ Outputs pixel coordinates of the corners for use in homography mapping (pixel â†
 
 import cv2
 
+def pick_corners(camera_i=1):
+
 # connect camera
-cap = cv2.VideoCapture(1)
-
-# window for camera
-cv2.namedWindow("Frame")
-
-# this function will be called when you click
-def click_event(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(f"Clicked coordinates: ({x}, {y})")
-
-# set the mouse callback on the window
-cv2.setMouseCallback("Frame", click_event)
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    cap = cv2.VideoCapture(1)
     
-    cv2.imshow("Frame", frame)
-    
-    # Press 'q' to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # window for camera
+    cv2.namedWindow("Frame")
+    corners=[]
 
-cap.release()
-cv2.destroyAllWindows()
+    # this function will be called when you click
+    def click_event(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            print(f"Clicked coordinates: ({x}, {y})")
+            corners.append((x,y))
+    
+    # set the mouse callback on the window
+    cv2.setMouseCallback("Frame", click_event)
+    
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        cv2.imshow("Frame", frame)
+        
+        # Press 'q' to quit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
+    return corners
